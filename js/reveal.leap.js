9375c2d922516e58dec6ebd4a610be7765fc54c0
Samuli Hakoniemi <samuli@hakoniemi.net>
2013-09-23 20:40:46 +0300

    Clean unnecessary code, add direction threshold as a variable
    Cleaning parts of the code that are not needed + adding a "direction threshold" as a variable.

--- js/reveal.leap.js
+++ js/reveal.leap.js
@@ -4,13 +4,13 @@
  * @author https://github.com/jefBinomed
  */
 
-var RevealLeap = function() {
+var RevealLeap = (function() {
+    var threshold = 0.9;
+    var lastTime = -1;
+    var frameRate = 0;
 
     // Support both the WebSocket and MozWebSocket objects
-    if ((typeof(WebSocket) == 'undefined') &&
-    (typeof(MozWebSocket) != 'undefined')) {
-        WebSocket = MozWebSocket;
-    }
+    WebSocket = WebSocket || MozWebSocket || {};
 
     ws = new WebSocket("ws://localhost:6437/");
     ws.onopen = function(event) {
@@ -18,29 +18,25 @@ var RevealLeap = function() {
          var enableMessage = JSON.stringify({enableGestures: true});
          ws.send(enableMessage); // Enable gestures
     }
-    var lastTime = -1;
-    var frameRate = 0;
 
     ws.onmessage = function(event) {
         frameRate++;
-        if(0 === frameRate%5) {
+        if(0 === frameRate % 5) {
             var obj = JSON.parse(event.data);
-            if (obj.gestures){
-                if(obj.gestures.length > 0) {
-                    var gesture = obj.gestures[0];
-                    if("swipe" === gesture.type) {
-                        var currentTime = new Date().getTime();
-                        if(-1 == lastTime){
-                            lastTime = currentTime;
-                            if(gesture.direction[0] < -0.9){ // horizontal swipes only
+            if (obj.gestures && obj.gestures.length){
+                var gesture = obj.gestures[0];
+                if ("swipe" === gesture.type) {
+                    var currentTime = new Date().getTime();
+                    if (-1 == lastTime){
+                        lastTime = currentTime;
+                        if (gesture.direction[0] < -threshold) { // horizontal swipes only
                             Reveal.prev();
-                            } else if (gesture.direction[0] > 0.9) {
-                              Reveal.next();
-                            }
+                        } else if (gesture.direction[0] > threshold) {
+                            Reveal.next();
                         }
-                        else if (300 < currentTime - lastTime) {
-                            lastTime = -1;
-                        }
+                    }
+                    else if (300 < currentTime - lastTime) {
+                        lastTime = -1;
                     }
                 }
             }
@@ -54,4 +50,4 @@ var RevealLeap = function() {
     ws.onerror = function(event) {
         console.log("ERROR: " + event);
     }
-}();
+}());
